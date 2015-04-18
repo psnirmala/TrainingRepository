4806bee1b9ff25c7f690387c20f692fde1637ae2
unknown <ps@test.com>
2015-04-18 15:41:21 +0530

    first update

--- src/main/java/com/springpeople/App.java
+++ src/main/java/com/springpeople/App.java
@@ -1,13 +1,22 @@
 package com.springpeople;
 
 /**
- * Hello world!
+ * This is App class
  *
  */
-public class App
+ public class App
 {
+
+	/** Main method */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!  --- with github" );
     }
+
+    /** This is a sample method */
+    public void sample()
+    {
+		System.out.println("test");
+	}
+
 }
